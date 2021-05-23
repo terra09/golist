852e675cbd45ca495a148b92ae3fc6b40290a1a6
pirate <terra0009@gmail.com>
2021-05-23 12:30:55 -0300

    Get dummie lists
    * requestDesc.txt
    Text file that describe the main functionalities that should be implemented.
    
    * golist.go
    - Define the list type, which is composed by an ID and items.
    Items are composed by a name and boolean that represent if it has been
    checked or not.
    - Create a function that returns a dummy list of lists.
    - Now handle access to /lists, where the list of lists is printed (both at
    the page and at the terminal)
    
    Interesting notes:
    	We can annotate variables to inform which name they should take when
    converted to json.
    	Go treats variables in a structure starting by an upper case letter
    as public and a lower case as private. Private variables are ignored when
    the structure is being encoded to json. (I lost quite the amount of time trying
    to figure out why some variables were being ignored).

--- golist.go
+++ golist.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type GoItem struct {
+	Name string `json:"name"`
+	Check bool `json:"check"`
+}
+
+type GoList struct {
+	ID int `json:"id"`
+	Items []GoItem `json:"items"`
+}
+
+func _dummieLists() []GoList {
+	lists := []GoList{
+		GoList{
+			ID: 1, Items: []GoItem{
+				GoItem{Name: "Banana", Check: false},
+				GoItem{Name: "Chuchu", Check: true},
+			},
+		},
+		GoList{
+			ID: 2, Items: []GoItem{
+				GoItem{Name: "chocolate", Check: false},
+				GoItem{Name: "miojo", Check: true},
+			},
+		},
+	}
+	return lists
+}
+
+func getLists(w http.ResponseWriter, r *http.Request) {
+	lists := _dummieLists()
+	json.NewEncoder(w).Encode(lists)
+	fmt.Println("getList has been accessed, result:")
+	jsonData, err := json.MarshalIndent(lists, "", "\t")
+	if err != nil { log.Println(err) }
+	fmt.Println(string(jsonData))
+}
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "golist server bootstrap!")
@@ -13,6 +51,7 @@ func rootPage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", rootPage)
+	http.HandleFunc("/lists", getLists)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
